97a4caf2752e0bdb1090d0e992f6d3eebfa5e778
Samaki01 <andrea.quaglia01@gmail.com>
2023-12-04 15:56:38 +0100

    Complete test1

--- src/it/samaki/adventOfCode2023/problem4/Problem4.java
+++ src/it/samaki/adventOfCode2023/problem4/Problem4.java
@@ -3,6 +3,7 @@ package it.samaki.adventOfCode2023.problem4;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.Arrays;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -16,17 +17,27 @@ public class Problem4 {
         Pattern pattern = Pattern.compile("\\d+");
         Matcher matcher;
 
-        final int NUMBER_WINNING_NUMBERS = 10;
-        final int NUMBER_NUMBERS_YOU_HAVE = 25;
+//        final int NUMBER_WINNING_NUMBERS = 10;
+//        final int NUMBER_NUMBERS_YOU_HAVE = 25;
+//        final int MAX_NUMBER_OF_CARDS = 204;
+
+        final int NUMBER_WINNING_NUMBERS = 5;
+        final int NUMBER_NUMBERS_YOU_HAVE = 8;
+        final int MAX_NUMBER_OF_CARDS = 6;
 
         int[] winningNumbers = new int[NUMBER_WINNING_NUMBERS];
         int[] numbersYouHave = new int[NUMBER_NUMBERS_YOU_HAVE];
+        int[] numberOfCopiesOfCards = new int[MAX_NUMBER_OF_CARDS];
 
         int currentCardPoints = 0;
         int totalCardPoints = 0;
+        int currentCardNumber = 0;
+        int totalNumberOfCards = 0;
+
+        Arrays.fill(numberOfCopiesOfCards, 1);
 
         try (BufferedReader bufferedReader = new BufferedReader(new FileReader(
-                "./res/it/samaki/adventOfCode2023/problem4/test2.txt"))) {
+                "./res/it/samaki/adventOfCode2023/problem4/test1.txt"))) {
             //For each card:
             while ((currentLine = bufferedReader.readLine()) != null) {
                 matcher = pattern.matcher(currentLine);
@@ -58,8 +69,32 @@ public class Problem4 {
 
                 totalCardPoints += currentCardPoints;
                 currentCardPoints = 0;
+
+                //Calculate number of winning numbers in the card.
+                for (int number : numbersYouHave) {
+                    for (int winningNumber : winningNumbers) {
+                        if (number == winningNumber)
+                            currentCardPoints++;
+                    }
+                }
+
+                if (matcher.find(0))
+                    currentCardNumber = Integer.parseInt(matcher.group());
+
+                //Calculate number of copies won.
+                for (int i = numberOfCopiesOfCards[currentCardNumber - 1]; i > 0; i--) {
+                    for (int j = currentCardNumber; j < currentCardNumber + currentCardPoints; j++) {
+                        if (j >= MAX_NUMBER_OF_CARDS)
+                            break;
+                        numberOfCopiesOfCards[j]++;
+                    }
+                }
             }
         }
+        for (int i = 0; i < MAX_NUMBER_OF_CARDS; i++)
+            totalNumberOfCards += numberOfCopiesOfCards[i];
+
+        System.out.println("The total number of scratchcards do we end up with is: " + totalNumberOfCards);
         System.out.println("The pile of cards is worth " + totalCardPoints + " points in total.");
     }
 }
