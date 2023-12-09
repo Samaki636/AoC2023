1bde968f8592d494c4985e0ac278f8ef9038baf0
Samaki01 <andrea.quaglia01@gmail.com>
2023-12-09 22:49:36 +0100

    Setup problem9 part1

---
+++ src/it/samaki/adventOfCode2023/problem9/Problem9.java
@@ -0,0 +1,16 @@
+package it.samaki.adventOfCode2023.problem9;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+/**
+ * @author : Samaki01
+ **/
+public class Problem9 {
+    public static void main (String[] args) throws FileNotFoundException {
+        try (Scanner scanner = new Scanner(new File("./res/it/samaki/adventOfCode2023/problem8/test3.txt"))) {
+
+        }
+    }
+}
