940996579c7e5d6ddf692ad5da80ac60269c6817
Samaki01 <andrea.quaglia01@gmail.com>
2023-12-12 11:26:59 +0100

    Setup problem12 part1

---
+++ src/it/samaki/adventOfCode2023/problem12/Problem12.java
@@ -0,0 +1,16 @@
+package it.samaki.adventOfCode2023.problem12;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+/**
+ * @author : Samaki01
+ **/
+public class Problem12 {
+    public static void main (String[] args) throws FileNotFoundException {
+        try (Scanner scanner = new Scanner(new File("./res/it/samaki/adventOfCode2023/problem12/test1.txt"))) {
+
+        }
+    }
+}
