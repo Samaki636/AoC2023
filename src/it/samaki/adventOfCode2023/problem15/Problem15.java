4dfdce4cb4b704d66e0a454cf63e9ca502085679
Samaki01 <andrea.quaglia01@gmail.com>
2023-12-16 08:24:01 +0100

    Setup problem 15 part1

---
+++ src/it/samaki/adventOfCode2023/problem15/Problem15.java
@@ -0,0 +1,16 @@
+package it.samaki.adventOfCode2023.problem15;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+/**
+ * @author : Samaki01
+ **/
+public class Problem15 {
+    public static void main(String[] args) throws FileNotFoundException {
+        try (Scanner scanner = new Scanner(new File("res/it/samaki/adventOfCode2023/problem15/test1.txt"))) {
+
+        }
+    }
+}
