ed15f4ca35b7af22affcabfa2b0b6c07d6d14ca8
Samaki01 <andrea.quaglia01@gmail.com>
2023-12-16 08:43:53 +0100

    Complete problem 15 part1

--- src/it/samaki/adventOfCode2023/problem15/Problem15.java
+++ src/it/samaki/adventOfCode2023/problem15/Problem15.java
@@ -9,8 +9,27 @@ import java.util.Scanner;
  **/
 public class Problem15 {
     public static void main(String[] args) throws FileNotFoundException {
-        try (Scanner scanner = new Scanner(new File("res/it/samaki/adventOfCode2023/problem15/test1.txt"))) {
-
+        String tmp;
+        try (Scanner scanner = new Scanner(new File("res/it/samaki/adventOfCode2023/problem15/test2.txt"))) {
+            tmp = scanner.nextLine();
         }
+        String[] input = tmp.split(",");
+
+        //apply ash function
+        int currentValue;
+        int sum = 0;
+        for (String s : input) {
+            currentValue = 0;
+            for (char c : s.toCharArray()) {
+                if (c == '\n') continue;
+                currentValue += c;
+                currentValue *= 17;
+                currentValue %= 256;
+            }
+            System.out.println("Current value :" + currentValue);
+            sum += currentValue;
+        }
+
+        System.out.println("The sum of the results is: " + sum);
     }
 }
